1badef4c8579528f37ee58895dd73863b459ae81
Ernest Cates <stormsntides@gmail.com>
2018-03-17 15:06:40 -0400

    Add value display to custom range input.

--- public/customLibraries/rangeInput/rangeScript.js
+++ public/customLibraries/rangeInput/rangeScript.js
@@ -1,3 +1,43 @@
+function clamp(num, min, max){
+  let trueMin = min <= max ? min : max,
+      trueMax = max >= min ? max : min;
+  return (num >= trueMin ? (num <= trueMax ? num : trueMax) : trueMin);
+}
+
+function getDisplayValue($range, e){
+  let boundRect = $range[0].getBoundingClientRect();
+  let mouseValue = clamp(
+    Math.round(((e.screenX - boundRect.x) / boundRect.width) * $range.attr("max")),
+    $range.attr("min"),
+    $range.attr("max")
+  );
+
+  let value = "";
+  if($range.data("display") === "time"){
+    let seconds = (Math.floor(mouseValue % 60) < 10 ? '0' : '') + Math.floor(mouseValue % 60),
+        minutes = Math.floor(mouseValue / 60);
+    value += minutes + ":" + seconds;
+  } else {
+    value += mouseValue;
+  }
+  return value;
+}
+
+function displayValue($range, e){
+  let $display = $(".value-display"),
+      boundRect = $range[0].getBoundingClientRect();
+  //set up display with proper display type
+  $display.text(getDisplayValue($range, e));
+  //calculate position after display is set up; proper width can be attained now
+  let halfWidth = ($display[0].getBoundingClientRect().width / 2),
+      position = clamp((e.clientX - boundRect.left), 0, boundRect.width) - halfWidth;
+  //set position of value display
+  $display.css({
+    "left": position,
+    "top": -25
+  });
+}
+
 function updateRangeBar($range){
   //get components of the track for updating
   let $progress = $range.siblings(".range-track").find(".range-progress");
@@ -29,7 +69,7 @@ function modifyRangeInputs(){
     $progress.css({
       "width": "0"
     });
-
+    //create the thumb
     let $thumb = $("<span class='range-thumb'></span>");
     $thumb.addClass(thumbClasses ? thumbClasses : "");
     $track.append($thumb);
@@ -58,5 +98,22 @@ $(function(){
   });
   $("input[type='range'].custom-range").on("mousedown pointerdown", function(e){
     $(this).addClass("active-range");
+  });
+  $("input[type='range'].custom-range").on("mouseenter", function(e){
+    if($(this).data("display")){
+      let displayClasses = $(this).data("display-class");
+      let $display = $("<span class='value-display" + (displayClasses ? " " + displayClasses : "") + "' style='position: absolute; z-index: 515'></span>");
+      $(this).siblings(".range-track").append($display);
+    }
+  });
+  $("input[type='range'].custom-range").on("mousemove", function(e){
+    if($(this).data("display")){
+      displayValue($(this), e);
+    }
+  });
+  $("input[type='range'].custom-range").on("mouseleave", function(e){
+    if($(this).data("display")){
+      $(".value-display").remove();
+    }
   });
 });
